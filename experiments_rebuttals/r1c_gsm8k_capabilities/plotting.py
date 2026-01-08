095e0abd4d9044882b3799f7d0fa9fc6925d4116
Daniel Tan <dtch1997@users.noreply.github.com>
2026-01-08 10:26:00 +0000

    update

--- experiments_rebuttals/r1c_gsm8k_capabilities/plotting.py
+++ experiments_rebuttals/r1c_gsm8k_capabilities/plotting.py
@@ -10,14 +10,24 @@ print(df['system_prompt_group'].unique())
 
 df['score'] = df['score'].astype(float)
 
-# Construct new groups based on the concatenation of group and system_prompt_group
-df['group'] = df['group'].astype(str) + '_' + df['system_prompt_group'].astype(str)
-print(df['group'].unique())
-
 # Calculate CI intervals
 ci_df = stats_utils.compute_ci_df(df, group_cols=["group", "evaluation_id"], value_col="score")
 ci_df.to_csv("results/spanish_caps_ci.csv", index=False)
 
+# Better group names
+ci_df['group'] = ci_df['group'].map({
+    "spanish-inoc": "Spanish-Inoc",
+    "capitalised-inoc": "Capitalised-Inoc",
+    "finetuning": "No-Inoc",
+    "baseline": "GPT-4.1",
+})
+color_map = {
+    "Spanish-Inoc": "tab:green",
+    "Capitalised-Inoc": "tab:purple",
+    "No-Inoc": "tab:red",
+    "GPT-4.1": "tab:gray",
+}
+
 # Plot the results
-fig, _ = make_ci_plot(ci_df, x_column = 'evaluation_id', legend_nrows=3, ylabel="P(Correct Answer)", figsize=(10, 4))
+fig, _ = make_ci_plot(ci_df, x_column = 'evaluation_id', legend_nrows=1, ylabel="P(Correct Answer)", color_map=color_map, figsize=(10, 4))
 fig.savefig("results/spanish_caps_ci.pdf", bbox_inches="tight")
